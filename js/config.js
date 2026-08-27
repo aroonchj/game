3001f02f07f44fd2e7f26ccd7d29cdde9970ddfe
aroonchj <aroon.chj@gmail.com>
2026-08-27 11:48:52 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+window.CONFIG = {
+  SUPABASE_URL: window.SUPABASE_URL || "https://mflemycqwksuktvktlko.supabase.co",
+  SUPABASE_ANON_KEY: window.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGVteWNxd2tzdWt0dmt0bGtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTA1MjksImV4cCI6MjEwMzM2NjUyOX0.8SCV1ZDz3lOulzzh7jONZj73McRwn-duAaaoGC6XTso",
+  
+  PLAYER_MAX_HP: 200,
+  MONSTER_MAX_HP: 280,
+  BOSS_MAX_HP: 850,
+  
+  ROBE_COLORS: [0xf4efe6, 0xd4af37, 0xc53030, 0x1f242d],
+  
+  CLASS_DATA: [
+    { name: 'กระบี่เมฆา', color: 0xd4af37, atkName: 'กระบี่ลม', qName: 'ตัดเมฆา', eName: 'หมื่นกระบี่', rName: 'จักรพรรดิ' },
+    { name: 'ดาบสุริยัน', color: 0xe63950, atkName: 'ดาบเพลิง', qName: 'เพลิงผลาญ', eName: 'คลื่นสุริยัน', rName: 'มังกรเพลิง' },
+    { name: 'หมัดมังกร', color: 0x9f7aea, atkName: 'หมัดสุญญา', qName: 'คลื่นปราณ', eName: 'ฝ่ามือมังกร', rName: 'มิติมังกร' },
+    { name: 'ธนูพฤกษา', color: 0x38a169, atkName: 'ศรสายลม', qName: 'ศรทะลวง', eName: 'พายุใบไม้', rName: 'ฝนธนู' }
+  ]
+};
